ce17b776b5ea1735b33bf602a6aac69ac745fe59
unknown <joseph.schabler@mymail.champlain.edu>
2026-01-28 11:06:44 -0500

    Fixed Week 2 submodule issue

---
+++ Week 2/Process Management #4.ps1
@@ -0,0 +1,8 @@
+﻿if (Get-Process -Name "chrome" -ErrorAction SilentlyContinue) {
+    Stop-Process -Name "chrome" -Force
+    Write-Host "Google Chrome was running and has been stopped."
+}
+else {
+    Start-Process "chrome.exe" "https://www.champlain.edu/"
+    Write-Host "Google Chrome was not running. Starting now and directing to champlain.edu"
+}
