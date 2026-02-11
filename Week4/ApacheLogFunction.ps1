fc6023f37eab2773ceaccc9f86077acd27919c88
unknown <joseph.schabler@mymail.champlain.edu>
2026-02-11 09:41:37 -0500

    Completed Apache Log Parsing Assignment

---
+++ Week4/ApacheLogFunction.ps1
@@ -0,0 +1,27 @@
+﻿function ApacheLogs1 
+{
+
+$logPath = "C:\xampp\apache\logs\access.log"
+$rawLogs = Get-Content $logPath
+
+$results = foreach ($line in $rawLogs)
+{
+        $parts = $line.Split(" ")
+
+        $referrerValue = if ($parts.Count -gt 10) { $parts[10].Replace('"','') } else { "-" }
+
+        [PSCustomObject]@{
+                IP       = $parts[0]
+                Time     = $parts[3].TrimStart('[')
+                Method   = $parts[5].TrimStart('"')
+                Page     = $parts[6]
+                Protocol = $parts[7].TrimEnd('"')
+                Response = $parts[8]
+                Referrer = $referrerValue
+            }
+        }
+    $filteredResults = $results | Where-Object { $_.IP -ilike "10.*" }
+    
+    return $filteredResults
+}
+
---
+++ Week4/main.ps1
@@ -0,0 +1,8 @@
+﻿. .\ApacheLogFunction.ps1
+
+$tableRecords = ApacheLogs1
+
+Write-Host ($tableRecords | Format-Table -AutoSize | Out-String)
+
+Write-Host ($tableRecords | Where-Object { $_.Page -like "*page2.html*" -and $_.Referrer -like "*index.html*" } |
+                                         Format-Table -AutoSize | Out-String)
